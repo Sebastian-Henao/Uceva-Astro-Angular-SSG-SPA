63b4a07427d5206f4851c6aa27df0b9963049dea
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-23 13:22:32 -0500

    feat(Angulas:marvel-rivals): Se configura la pagina de MarvelRivals

--- Client-Angular/src/app/pages/MarvelRivals/marvel-rivals.page.ts
+++ Client-Angular/src/app/pages/MarvelRivals/marvel-rivals.page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MarvelRivalsTableComponent } from '../../components/MarvelRivals-table/marvel-rivals-table.component';
+import { AlertComponent } from '../../components/alert/alert.component';
+import { MarvelRivals } from '../../interfaces/MarvelRivals.interface';
+import { State } from '../../interfaces/state.interface';
+import { MarvelRivalsService } from '../../services/MarvelRivals/marvel-rivals.service';
 
 @Component({
-  selector: 'app-marvel-rivals.page',
-  imports: [],
+  selector: 'app-marvel-rivals',
+  imports: [MarvelRivalsTableComponent, AlertComponent],
   templateUrl: './marvel-rivals.page.html',
-  styleUrl: './marvel-rivals.page.scss',
 })
 export class MarvelRivalsPage {
-
+  Marvelrivals: MarvelRivals[] = [];
+    state: State = 'init';
+  private marvelRivalsService = inject(MarvelRivalsService);
+  ngOnInit(): void {
+    this.state = 'loading';
+    this.marvelRivalsService.getAllMarvelRivals().subscribe({
+      next: (Marvelrivals) => {
+        this.Marvelrivals = Marvelrivals;
+        this.state = 'success';
+      },
+      error: (error) => {
+        console.error(error)
+        this.state = 'error';
+      },
+    })
+  }
 }
