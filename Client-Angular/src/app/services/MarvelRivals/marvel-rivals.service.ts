78b7bf64babee415cab4396e8fbe4e4011493dcf
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-23 13:12:45 -0500

    feat(Angulas:marvel-rivals): Se crea el servicio para vista MarvelRivals

--- Client-Angular/src/app/services/MarvelRivals/marvel-rivals.service.ts
+++ Client-Angular/src/app/services/MarvelRivals/marvel-rivals.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MarvelRivals } from '../../interfaces/MarvelRivals.interface';
+import { PERSONAJES_MARVEL_RIVALS } from '../../data/MarvelRivals.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarvelRivalsService {
-  
+  getAllMarvelRivals(): Observable<MarvelRivals[]> {
+    return of(PERSONAJES_MARVEL_RIVALS);
+  }
 }
