0ba31fd3ac552a535510fdfea9103dd659431cdc
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-23 12:56:18 -0500

    feat(Angulas:marvel-rivals): Se realiza la configuracion del componente para la tabla

--- Client-Angular/src/app/components/MarvelRivals-table/marvel-rivals-table.component.ts
+++ Client-Angular/src/app/components/MarvelRivals-table/marvel-rivals-table.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MarvelRivals, RolCategoria } from '../../interfaces/MarvelRivals.interface';
+import { BadgeAtom, BadgeType } from '@brejcha13320/design-system-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-marvel-rivals-table.component',
-  imports: [],
+  selector: 'app-marvel-rivals-table',
+  imports: [CommonModule, BadgeAtom],
   templateUrl: './marvel-rivals-table.component.html',
-  styleUrl: './marvel-rivals-table.component.scss',
 })
 export class MarvelRivalsTableComponent {
+  @Input() marvelrivals: MarvelRivals[] = [];
 
+  categoryMap: Record<RolCategoria, BadgeType> = {
+    'Vanguardia': 'primary',
+    'Duelista': 'danger',
+    'Estratega': 'info',
+    'Variable': 'warning',
+  }
 }
