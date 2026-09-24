34fc5b9578362af1e36fd37f15bdd1b2f2d08d2a
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-23 20:54:55 -0500

    test(Angular: MarvelRivals): Se completan pruebas unitarias

--- Client-Angular/src/app/services/MarvelRivals/marvel-rivals.service.spec.ts
+++ Client-Angular/src/app/services/MarvelRivals/marvel-rivals.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MarvelRivalsService } from './marvel-rivals.service';
+import { PERSONAJES_MARVEL_RIVALS } from '../../data/MarvelRivals.interface';
 
 describe('MarvelRivalsService', () => {
   let service: MarvelRivalsService;
@@ -10,7 +11,17 @@ describe('MarvelRivalsService', () => {
     service = TestBed.inject(MarvelRivalsService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('Creacion del servicio', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('getAllMarvelRivals deberia retornar un observable con los personajes', (done) => {
+      service.getAllMarvelRivals().subscribe(marvelRivals => {
+        expect(marvelRivals).toEqual(PERSONAJES_MARVEL_RIVALS);
+        expect(marvelRivals.length).toBe(PERSONAJES_MARVEL_RIVALS.length);
+        done();
+      });
+    });
   });
 });
