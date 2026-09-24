5fb915df61ef510cdecebca0ea58c61491bdbdbb
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-24 17:06:19 -0500

    feat(angular/lol-jungle): Se crea el mock con los 20 junglas

---
+++ Client-Angular/src/app/interfaces/JungleChampion.ts
@@ -0,0 +1,7 @@
+export interface JungleChampion {
+  id: string;
+  name: string;
+  title: string;
+  class: string;
+  region: string;
+}
---
+++ Client-Angular/src/app/mocks/JungleChampions.ts
@@ -0,0 +1,24 @@
+import { JungleChampion } from '../interfaces/JungleChampion';
+
+export const JUNGLE_CHAMPIONS: JungleChampion[] = [
+  { id: 'lee-sin', name: 'Lee Sin', title: 'El Monje Ciego', class: 'Luchador', region: 'Jonia' },
+  { id: 'elise', name: 'Elise', title: 'La Reina de las Arañas', class: 'Mago', region: 'Islas de la Sombra' },
+  { id: 'khazix', name: "Kha'Zix", title: 'El Saqueador del Vacío', class: 'Asesino', region: 'El Vacío' },
+  { id: 'rengar', name: 'Rengar', title: 'El Orgullo Acechante', class: 'Asesino', region: 'Ixtal' },
+  { id: 'jarvan-iv', name: 'Jarvan IV', title: 'El Ejemplo de Demacia', class: 'Tanque', region: 'Demacia' },
+  { id: 'vi', name: 'Vi', title: 'La Vigilante de Piltóver', class: 'Luchador', region: 'Piltóver' },
+  { id: 'warwick', name: 'Warwick', title: 'La Ira Desencadenada de Zaun', class: 'Luchador', region: 'Zaun' },
+  { id: 'nocturne', name: 'Nocturne', title: 'La Pesadilla Eterna', class: 'Asesino', region: 'Runaterra' },
+  { id: 'amumu', name: 'Amumu', title: 'La Momia Triste', class: 'Tanque', region: 'Shurima' },
+  { id: 'graves', name: 'Graves', title: 'El Forajido', class: 'Tirador', region: 'Aguasturbias' },
+  { id: 'kindred', name: 'Kindred', title: 'Los Cazadores Eternos', class: 'Tirador', region: 'Runaterra' },
+  { id: 'master-yi', name: 'Master Yi', title: 'La Espada Wuju', class: 'Asesino', region: 'Jonia' },
+  { id: 'hecarim', name: 'Hecarim', title: 'La Sombra de la Guerra', class: 'Luchador', region: 'Islas de la Sombra' },
+  { id: 'sejuani', name: 'Sejuani', title: 'La Furia del Norte', class: 'Tanque', region: 'Freljord' },
+  { id: 'zac', name: 'Zac', title: 'El Arma Secreta', class: 'Tanque', region: 'Zaun' },
+  { id: 'nidalee', name: 'Nidalee', title: 'La Cazadora Bestial', class: 'Asesino', region: 'Ixtal' },
+  { id: 'evelynn', name: 'Evelynn', title: 'El Abrazo de la Agonía', class: 'Asesino', region: 'Runaterra' },
+  { id: 'shaco', name: 'Shaco', title: 'El Bufón Siniestro', class: 'Asesino', region: 'Runaterra' },
+  { id: 'viego', name: 'Viego', title: 'El Rey Arruinado', class: 'Asesino', region: 'Islas de la Sombra' },
+  { id: 'lillia', name: 'Lillia', title: 'La Flor Tímida', class: 'Luchador', region: 'Jonia' }
+];
