f8a221e9000ad7e0e9b19ac613d12302a064e4f6
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-24 17:04:04 -0500

    feat(angular/lol-jungle): Se crean carpetas y componentes base para la vista

---
+++ Client-Angular/src/app/components/lol-jungle-table/lol-jungle-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lol-jungle-table',
+  imports: [],
+  templateUrl: './lol-jungle-table.html',
+  styleUrl: './lol-jungle-table.scss',
+})
+export class LolJungleTable {
+
+}
---
+++ Client-Angular/src/app/pages/lol-jungle/lol-jungle-page/lol-jungle-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lol-jungle-page',
+  imports: [],
+  templateUrl: './lol-jungle-page.html',
+  styleUrl: './lol-jungle-page.scss',
+})
+export class LolJunglePage {
+
+}
